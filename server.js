9108448f416da49a0eeca58a5cc71505f248349d
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-05-17 16:58:10 +0200

    Validation practice

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/validation-codealong"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+// Create model for mongo database, using mongoose
+const Person = mongoose.model('Person', {
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 500
+  },
+  height: {
+    type: Number,
+    required: true,
+    min: 5
+  },
+  birthdate: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+new Person({ name: "Lisa", height: 150, }).save()
+
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -16,6 +39,34 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+/* app.post('/people', async (req, res) => {
+  const person = new Person(req.body)
+  const savedPerson = await person.save()
+  res.json(savedPerson)
+}) */
+
+// Refactored from above. Two ways of writing validation.
+app.post('/people', async (req, res) => {
+  // Promises
+  new Person(req.body).save()
+    .then((person) => {
+      res.status(200).json(person)
+    })
+    .catch((err) => {
+      res.status(400).json({ message: 'Could not save person', errors: err.errors })
+    })
+
+  // Try catch form
+  try {
+    // Success
+    const person = await new Person(req.body).save()
+    res.status(200).json(person)
+  } catch (err) {
+    // Bad request
+    res.status(400).json({ message: 'Could not save person', errors: err.errors })
+  }
 })
 
 // Start the server
